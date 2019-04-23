2e7cc219d889ace85aa69e966ba297b1410ac953
David Greenberg <david@grefin.com>
2019-04-23 08:54:55 -0700

    Added index file for npm.

---
+++ index.js
@@ -0,0 +1,8 @@
+var crownpeakAPI = require("./CrownpeakAPI/API");
+
+module.exports = {
+    Api: crownpeakAPI.api,
+    AccessAsset: crownpeakAPI.AccessAsset,
+    Util: crownpeakAPI.Util,
+    API: crownpeakAPI.api
+}
